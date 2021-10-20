468fb4cc81b10cc929626adb9e66e2b3ae768ce4
rlawo0307 <rlawo0307@naver.com>
2021-10-20 15:47:49 +0900

    .

--- Game.Designer.cs
+++ Game.Designer.cs
@@ -35,7 +35,13 @@ namespace MineSweeper
             this.초급ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.중급ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.고급ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PictureBox_Bomb = new System.Windows.Forms.PictureBox();
+            this.Label_Bomb = new System.Windows.Forms.Label();
+            this.PictureBox_Time = new System.Windows.Forms.PictureBox();
+            this.Label_Time = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Bomb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Time)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -71,33 +77,73 @@ namespace MineSweeper
             // 초급ToolStripMenuItem
             // 
             this.초급ToolStripMenuItem.Name = "초급ToolStripMenuItem";
-            this.초급ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.초급ToolStripMenuItem.Size = new System.Drawing.Size(150, 34);
             this.초급ToolStripMenuItem.Text = "초급";
             this.초급ToolStripMenuItem.Click += new System.EventHandler(this.초급ToolStripMenuItem_Click);
             // 
             // 중급ToolStripMenuItem
             // 
             this.중급ToolStripMenuItem.Name = "중급ToolStripMenuItem";
-            this.중급ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.중급ToolStripMenuItem.Size = new System.Drawing.Size(150, 34);
             this.중급ToolStripMenuItem.Text = "중급";
             // 
             // 고급ToolStripMenuItem
             // 
             this.고급ToolStripMenuItem.Name = "고급ToolStripMenuItem";
-            this.고급ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.고급ToolStripMenuItem.Size = new System.Drawing.Size(150, 34);
             this.고급ToolStripMenuItem.Text = "고급";
+            // 
+            // PictureBox_Bomb
+            // 
+            this.PictureBox_Bomb.Location = new System.Drawing.Point(182, 45);
+            this.PictureBox_Bomb.Name = "PictureBox_Bomb";
+            this.PictureBox_Bomb.Size = new System.Drawing.Size(100, 50);
+            this.PictureBox_Bomb.TabIndex = 5;
+            this.PictureBox_Bomb.TabStop = false;
+            // 
+            // Label_Bomb
+            // 
+            this.Label_Bomb.AutoSize = true;
+            this.Label_Bomb.Location = new System.Drawing.Point(321, 76);
+            this.Label_Bomb.Name = "Label_Bomb";
+            this.Label_Bomb.Size = new System.Drawing.Size(54, 18);
+            this.Label_Bomb.TabIndex = 6;
+            this.Label_Bomb.Text = "label1";
+            // 
+            // PictureBox_Time
+            // 
+            this.PictureBox_Time.Location = new System.Drawing.Point(416, 45);
+            this.PictureBox_Time.Name = "PictureBox_Time";
+            this.PictureBox_Time.Size = new System.Drawing.Size(100, 50);
+            this.PictureBox_Time.TabIndex = 7;
+            this.PictureBox_Time.TabStop = false;
+            // 
+            // Label_Time
+            // 
+            this.Label_Time.AutoSize = true;
+            this.Label_Time.Location = new System.Drawing.Point(555, 76);
+            this.Label_Time.Name = "Label_Time";
+            this.Label_Time.Size = new System.Drawing.Size(54, 18);
+            this.Label_Time.TabIndex = 8;
+            this.Label_Time.Text = "label2";
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Label_Time);
+            this.Controls.Add(this.PictureBox_Time);
+            this.Controls.Add(this.Label_Bomb);
+            this.Controls.Add(this.PictureBox_Bomb);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Game";
             this.Text = "Game";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Bomb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Time)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,5 +156,9 @@ namespace MineSweeper
         private System.Windows.Forms.ToolStripMenuItem 초급ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 중급ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 고급ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox PictureBox_Bomb;
+        private System.Windows.Forms.Label Label_Bomb;
+        private System.Windows.Forms.PictureBox PictureBox_Time;
+        private System.Windows.Forms.Label Label_Time;
     }
 }
--- Game.cs
+++ Game.cs
@@ -12,9 +12,49 @@ namespace MineSweeper
 {
     public partial class Game : Form
     {
+        private int board_x = 10, board_y = 100;
+        private int button_width = 30, button_height = 30;
+        private Button[,] buttons;
+        private int[,] board;
+
         public Game()
         {
             InitializeComponent();
+
+            PictureBox_Bomb.Load(@"./res/bomb.png");
+            PictureBox_Bomb.SizeMode = PictureBoxSizeMode.StretchImage;
+            PictureBox_Time.Load(@"./res/time.png");
+            PictureBox_Time.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            int op = 1;
+            Init_Board(); // level-beginner
+            Play_Game();
+        }
+
+        private void Init_Board()
+        {
+            //MessageBox.Show(""+board.GetLength(0)+","+board.GetLength(1));
+            board = new int[5, 5]; // 자동으로 0으로 초기화
+            buttons = new Button[5, 5];
+            for (int i = 0; i < board.GetLength(0); i++)
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Left = board_x + j * button_width;
+                    buttons[i, j].Top = board_y + i * button_height;
+                    buttons[i, j].Width = button_width;
+                    buttons[i, j].Height = button_height;
+                    buttons[i,j].BackColor = System.Drawing.Color.Green;
+                    buttons[i, j].FlatStyle = FlatStyle.Flat;
+                    buttons[i, j].FlatAppearance.BorderSize = 0;
+                    buttons[i, j].Text = board[i, j].ToString();
+                    this.Controls.Add(buttons[i, j]);
+                }
+        }
+
+        private void Play_Game()
+        {
+            
         }
 
         private void 초급ToolStripMenuItem_Click(object sender, EventArgs e)
